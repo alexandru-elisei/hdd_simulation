c1c0bb0d22c90c4d7c67241ac193dc050ac64ff9
Alexandru Elisei <alexandru.elisei@gmail.com>
2015-03-21 19:41:04 +0200

    Changed invalid parameter name and message

--- types.c
+++ types.c
@@ -3,13 +3,13 @@
 void hdd_print_result(hdd_result_t msg)
 {
 	switch (msg) {
-	HDD_SUCCESS:
+	case HDD_SUCCESS:
 		fprintf(stderr, "HDD: Operation executed successfully.\n");
 		break;
-	HDD_ERROR_INVALID_PARAM:
+	case HDD_ERROR_INVALID_PARAMETER:
 		fprintf(stderr, "HDD: Invalid parameter supplied.\n");
 		break;
-	HDD_ERROR_MEMORY_ALLOC:
+	case HDD_ERROR_MEMORY_ALLOC:
 		fprintf(stderr, "HDD: Error allocating memory.\n");
 		break;
 	}
--- types.h
+++ types.h
@@ -8,9 +8,9 @@
 #include <stdlib.h>
 
 typedef enum {
-	HDD_SUCCESS		= 1, 	/* functions executed successfully */
-	HDD_ERROR_INVALID_PARAM	= 2,	/* parameter is invalid */
-	HDD_ERROR_MEMORY_ALLOC	= 3,	/* error while allocating memory */
+	HDD_SUCCESS			= 1, 	
+	HDD_ERROR_INVALID_PARAMETER	= 2,	/* parameter is invalid */
+	HDD_ERROR_MEMORY_ALLOC		= 3,	/* error allocating memory */
 } hdd_result_t;
 
 void hdd_print_result(hdd_result_t msg);
