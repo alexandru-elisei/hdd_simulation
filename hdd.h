85b40a107aa39db519501dfe7792f6dd4537e3b2
Alexandru Elisei <alexandru.elisei@gmail.com>
2015-03-22 17:20:57 +0200

    Added hdd_address structure

--- hdd.h
+++ hdd.h
@@ -7,6 +7,11 @@
 #undef SECTOR_SIZE 
 #define SECTOR_SIZE	5
 
+struct hdd_address {
+	int line;
+	int index;
+}
+
 /* Nonlinear linked list, each "level" is a circular list */
 struct hdd_sector {
 	struct hdd_sector *next;		/* Next sector in line */
@@ -16,20 +21,23 @@ struct hdd_sector {
 	unsigned int damage;
 };
 
+/* Drive head, reads the current sector and stores the position on the drive */
 struct hdd_head {
 	struct hdd_sector *sect;
-	uint8_t line;
-	unsigned int index;
+	struct hdd_address *addr;
 }; 
 
 /* Generates the hard drive */
 enum hdd_result hdd_init(struct hdd_sector **s, int lines);
 
 /* Prints the contents of the hard drive */
-enum hdd_result hdd_print(struct hdd_sector **s);
+enum hdd_result hdd_print(struct hdd_sector *s);
 
 /* The drive head is always initialized on sector 0 on line 0 */
-enum hdd_result hdd_head_init(struct hdd_head **h);
+enum hdd_result hdd_head_init(struct hdd_head **h, struct hdd_sector *s);
+
+/* Seeks an address by jumping one sector */
+enum hdd_result hdd_seek(int line, int sect, struct hdd_head *h);
 
 /* Frees allocated space */
 enum hdd_result hdd_dealocate(struct hdd_sector **s);
